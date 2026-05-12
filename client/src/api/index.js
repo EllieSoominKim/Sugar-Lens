b1c5b01ccfab63cd03026c6fe7a71a9cd1ea1213
EllieSoominKim <ellieskim06@gmail.com>
2026-05-12 16:41:41 +0900

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,4 @@
 export { scanFood } from './scanApi';
 export { getLibrary } from './libraryApi';
 export { getNutrition } from './nutritionApi';
-export { getRecommendations } from './recommendApi';
 export { sendChatMessage } from './chatbotApi';
