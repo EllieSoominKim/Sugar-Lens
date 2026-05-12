f206a49af69b7c5c6573debd6069fff47f71cb2c
EllieSoominKim <ellieskim06@gmail.com>
2026-05-12 16:28:07 +0900

    Create index.js

---
+++ client/src/api/index.js
@@ -0,0 +1,5 @@
+export { scanFood } from './scanApi';
+export { getLibrary } from './libraryApi';
+export { getNutrition } from './nutritionApi';
+export { getRecommendations } from './recommendApi';
+export { sendChatMessage } from './chatbotApi';
